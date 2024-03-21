46217f5e318af8f96c720a0ae81050b19bd266e2
wf <1982392655@qq.com>
2024-03-21 09:26:25 +0800

    修复打包问题

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "../dist/assets/AuxiliaryLine.worker-fcdb130f.js";
+import "./dist/style.css";
+// @ts-ignore
+import { SFEditor } from "../dist/svg-flow-editor-mvp.js";
+export { SFEditor };

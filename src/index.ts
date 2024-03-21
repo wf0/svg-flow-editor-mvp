fa385beb1b12e4d9fe136eb25ddbed000e0ef76a
wf <1982392655@qq.com>
2024-03-21 09:32:33 +0800

    优化打包

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import "../dist/assets/AuxiliaryLine.worker-fcdb130f.js";
-import "./dist/style.css";
-// @ts-ignore
-import { SFEditor } from "../dist/svg-flow-editor-mvp.js";
-export { SFEditor };
